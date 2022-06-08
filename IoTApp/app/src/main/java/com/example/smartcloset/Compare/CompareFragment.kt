7365c40b62e7b981820f255957681d656993c3f3
sys8517 <sys8517@naver.com>
2022-06-08 14:53:07 +0900

    add camera

--- IoTApp/app/src/main/java/com/example/smartcloset/Compare/CompareFragment.kt
+++ IoTApp/app/src/main/java/com/example/smartcloset/Compare/CompareFragment.kt
@@ -1,9 +1,13 @@
 package com.example.smartcloset.Compare
 
+import android.Manifest
 import android.app.Activity
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
+import android.icu.text.SimpleDateFormat
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -11,37 +15,138 @@ import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import com.example.smartcloset.Main.MainActivity
 import com.example.smartcloset.R
+import kotlinx.android.synthetic.main.addclothes.*
+import kotlinx.android.synthetic.main.compare.*
 import kotlinx.android.synthetic.main.compare.view.*
 import kotlinx.android.synthetic.main.compare_item.*
 import java.io.File
 import java.net.URI
 
+
 class CompareFragment: Fragment() {
     var uri : Uri? = null
     lateinit var mContext : Context
+    val PERMISSION_CAMERA = 1001 //맞나?
+    val REQUEST_CAMERA = 2 //맞나?
+    lateinit var realUri:Uri
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
+
+
     }
     var datalist =ArrayList<Int>()
     lateinit var mainActivity: MainActivity
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.compare, container, false)
         view.btn_add_comparePic.setOnClickListener {
             //등록 버튼 클릭 이벤트 리스너
+                requirePermissions(arrayOf(Manifest.permission.CAMERA), PERMISSION_CAMERA)
 
         }
+
+
         return view
 
 
     }
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun requirePermissions(permissions: Array<String>, requestCode: Int) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            permissionGranted(requestCode)
+        } else {
+            // isAllPermissionsGranted : 권한이 모두 승인 되었는지 여부 저장
+            // all 메서드를 사용하면 배열 속에 들어 있는 모든 값을 체크할 수 있다.
+            val isAllPermissionsGranted =
+                    permissions.all { ActivityCompat.checkSelfPermission(mainActivity,it) == PackageManager.PERMISSION_GRANTED }
+            if (isAllPermissionsGranted) {
+                permissionGranted(requestCode)
+            } else {
+                // 사용자에 권한 승인 요청
+                ActivityCompat.requestPermissions(mainActivity, permissions, requestCode)
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+            permissionGranted(requestCode)
+        } else {
+            permissionDenied(requestCode)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun permissionGranted(requestCode: Int) {
+        when (requestCode) {
+            PERMISSION_CAMERA -> openCamera()
+        }
+    }
+
+    private fun permissionDenied(requestCode: Int) {
+        when (requestCode) {
+            PERMISSION_CAMERA -> Toast.makeText(
+                mainActivity,
+                "카메라 권한을 승인해야 카메라를 사용할 수 있습니다.",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    //***************카메라
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun openCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        createImageUri(newFileName(), "image/jpg")?.let { uri ->
+            realUri = uri // var 맞나?
+            // MediaStore.EXTRA_OUTPUT을 Key로 하여 Uri를 넘겨주면
+            // 일반적인 Camera App은 이를 받아 내가 지정한 경로에 사진을 찍어서 저장시킨다.
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, realUri)
+            startActivityForResult(intent, REQUEST_CAMERA)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun newFileName(): String {
+        val sdf = SimpleDateFormat("yyyyMMdd_HHmmss")
+        val filename = sdf.format(System.currentTimeMillis())
+        return "$filename.jpg"
+    }
+
+    private fun createImageUri(filename: String, mimeType: String): Uri? {
+        var values = ContentValues()
+        values.put(MediaStore.Images.Media.DISPLAY_NAME, filename)
+        values.put(MediaStore.Images.Media.MIME_TYPE, mimeType)
+        return mainActivity.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == AppCompatActivity.RESULT_OK) {
+            when (requestCode) {
+                REQUEST_CAMERA -> {
+                    realUri?.let { uri ->
+                        img_compare_preview.setImageURI(uri)
+                    }
+                }
+            }
+        }
+    }
+
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
