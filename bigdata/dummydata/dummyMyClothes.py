9864643a280a2b02bea533105e37f527f6c698e1
power080900 <power080900@gmail.com>
2022-06-14 09:37:53 +0900

    DummyData update

--- bigdata/dummydata/dummyMyClothes.py
+++ bigdata/dummydata/dummyMyClothes.py
@@ -3,18 +3,27 @@ import random
 
 CODE = ['my'+str(i) for i in range(1,2001)]
 ClothesCODEList = ['CODE'+str(i) for i in range(1,101)]
-ClothesCODE = [random.choice(ClothesCODEList) for i in range(1,2001)]
 IDList = ['dummy'+str(i) for i in range(1,101)]
 ID = [random.choice(IDList) for i in range(1,2001)]
 BuyDateList = pd.date_range(start='20180101', end='20211231')
 BuyDate = [random.choice(BuyDateList) for i in range(1,2001)]
+colorList = ['red', 'blue', 'white', 'black', 'yellow']
+myColor = [random.choice(colorList) for i in range(1, 2001)]
+categoryList = ['jean', 'cotton', 'jogger', 'slacks', 'skirt',
+                'longPants', 'shortPants', 'bottomEtc', 'hoodie', 'knit',
+                'longShirts', 'shortShirts', 'sweatShirts', 'dressShirts','topEtc']
+myCategory = [random.choice(categoryList) for i in range(1, 2001)]
+myImg = ['img'+str(i) for i in range(1, 2001)]
+
 
 MyClothes = pd.DataFrame(
     {'CODE': CODE,
-     'ClothesCODE': ClothesCODE,
      'ID': ID,
-     'BuyDate': BuyDate,
-    }
+     'myColor': myColor,
+     'myCategory': myCategory,
+     'myImg': myImg,
+     'BuyDate': BuyDate
+     }
 )
 print(MyClothes)
 MyClothes.to_csv('dummyMyClothes.csv', encoding='utf-8')
--- bigdata/dummydata/dummyToday.py
+++ bigdata/dummydata/dummyToday.py
@@ -1,51 +1,23 @@
 import pandas as pd
 import random
-import numpy as np
 
-Clothes = pd.read_csv('dummyClothes.csv', encoding='Utf-8')
-MyClothes = pd.read_csv('dummyMyClothes.csv', encoding='Utf-8')
-UserData = pd.read_csv('dummyUser.csv', encoding='Utf-8')
-
-MyList = pd.merge(MyClothes, Clothes, left_on='ClothesCODE', right_on='ClothesCODE', how='left')
-MyList = pd.merge(MyList, UserData, left_on='ID', right_on='ID', how='left')
 TodayID = ['Today'+str(i) for i in range(1, 1001)]
-TodayList = pd.date_range(start='20220101', end='20220610')
-date = [random.choice(TodayList) for i in range(1, 1001)]
-
-i = 0
-dummy = []
-CodeTop = list()
-CodeBottom = list()
-
-while i <= 99:
-    i += 1
-    for j in range(1,11):
-        dummy = MyList.loc[MyList.ID == "dummy"+str(i), :]
-        TopClothes = dummy.loc[dummy.MainCategory == "top", ["CODE"]]
-        TopClothes = TopClothes.values.tolist()
-        TopClothes = np.array(TopClothes).flatten().tolist()
-        CodeTopList = [random.choice(TopClothes)]
-        CodeTop.append(CodeTopList)
-        BottomClothes = dummy.loc[dummy.MainCategory == "bottom", ["CODE"]]
-        BottomClothes = BottomClothes.values.tolist()
-        BottomClothes = np.array(BottomClothes).flatten().tolist()
-        CodeBottomList = [random.choice(BottomClothes)]
-        CodeBottom.append(CodeBottomList)
-        print(i," ",j)
-    print(CodeTop)
-    print(CodeBottom)
-
-CodeTop = np.array(CodeTop).flatten().tolist()
-CodeBottom = np.array(CodeBottom).flatten().tolist()
-print(CodeTop)
-print(CodeBottom)
-
+IDList = ['dummy'+str(i) for i in range(1, 101)]
+ID = [random.choice(IDList) for i in range(1, 1001)]
+TodayList = pd.date_range(start='20220401', end='20220610')
+today = [random.choice(TodayList) for i in range(1, 1001)]
+weatherList = ['sun', 'cloud', 'rain', 'fog']
+weather = [random.choice(weatherList) for i in range(1, 1001)]
+temp = [(random.randint(150, 300)/10) for i in range(1, 1001)]
+dust = [(random.randint(0, 1500)/10) for i in range(1, 1001)]
 
 TodayInfo = pd.DataFrame({
     'TodayID': TodayID,
-    'date': date,
-    'CodeTop': CodeTop,
-    'CodeBottom': CodeBottom
+    'ID': ID,
+    'today': today,
+    'weather': weather,
+    'temp': temp,
+    'dust': dust
 })
 print(TodayInfo)
 
