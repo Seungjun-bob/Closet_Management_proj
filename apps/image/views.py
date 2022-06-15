0d36da2530594ec76ee93099699130e9cd2269fc
YoonSub <crid2450@gmail.com>
2022-06-15 16:31:33 +0900

    Initial commit

---
+++ apps/image/views.py
@@ -0,0 +1,56 @@
+import io
+from PIL import Image as im
+import torch
+
+from django.shortcuts import render
+from django.views.generic.edit import CreateView
+
+from .models import ImageModel
+from .forms import ImageUploadForm
+
+
+class UploadImage(CreateView):
+    model = ImageModel
+    template_name = 'image/imagemodel_form.html'
+    fields = ["image"]
+
+    def post(self, request, *args, **kwargs):
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            img = request.FILES.get('image')
+            img_instance = ImageModel(
+                image=img
+            )
+            img_instance.save()
+
+            uploaded_img_qs = ImageModel.objects.filter().last()
+            img_bytes = uploaded_img_qs.image.read()
+            img = im.open(io.BytesIO(img_bytes))
+
+            path_hubconfig = "C:/Users/crid2/django_yolo_web/yolov5_code"
+            path_weightfile = "C:/Users/crid2/django_yolo_web/yolov5_code/train_file/yolov5s.pt"  # or any custom trained model
+
+            model = torch.hub.load(path_hubconfig, 'custom',
+                                   path=path_weightfile, source='local')
+
+            results = model(img, size=640)
+            results.render()
+            for img in results.imgs:
+                img_base64 = im.fromarray(img)
+                img_base64.save("media/yolo_out/image0.jpg", format="JPEG")
+
+            inference_img = "/media/yolo_out/image0.jpg"
+
+            form = ImageUploadForm()
+            context = {
+                "form": form,
+                "inference_img": inference_img
+            }
+            return render(request, 'image/imagemodel_form.html', context)
+
+        else:
+            form = ImageUploadForm()
+        context = {
+            "form": form
+        }
+        return render(request, 'image/imagemodel_form.html', context)
