2651d10c86def68fc54869697e822db5c27499ac
power080900 <power080900@gmail.com>
2022-06-17 10:28:47 +0900

    function update

--- bigdata/analysis/function.py
+++ bigdata/analysis/function.py
@@ -99,9 +99,9 @@ def compare(request):
     category = request.GET.get("category")
     color = request.GET.get("color")
     dummy = df[df['ID'] == userid]
-    compare_category = dummy[dummy['category'] == category and dummy['color'] == color].loc[:, ['category']]
-    compare_color = dummy[dummy['category'] == category and dummy['color'] == color].loc[:, ['color']]
-    compare_img = dummy[dummy['category'] == category and dummy['color'] == color].loc[:, ['img']]
+    compare_category = dummy[(dummy['myCategory'] == category) & (dummy['myColor'] == color)].loc[:, ['category']]
+    compare_color = dummy[(dummy['myCategory'] == category) & (dummy['myColor'] == color)].loc[:, ['color']]
+    compare_img = dummy[(dummy['myCategory'] == category) & (dummy['myColor'] == color)].loc[:, ['img']]
     context = {
         'compare_category': compare_category,
         'compare_color': compare_color,
