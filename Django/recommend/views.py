6473353c38caf4314634d30861961b9ac3412310
power080900 <power080900@gmail.com>
2022-06-21 16:59:15 +0900

    rcmd update

--- Django/recommend/views.py
+++ Django/recommend/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse, JsonResponse
 import pandas as pd
+import random
+import datetime
 
 def recommend(request) :
     name = request.GET.get('id', "")
@@ -60,3 +62,30 @@ def compare(request):
 
     return render(request, 'test2.html', context)
 
+def myclothes(request):
+    MyClothes = pd.DataFrame(
+        {'CODE': [],
+         'ID': [],
+         'myColor': [],
+         'myCategory': [],
+         'myImg': [],
+         'BuyDate': []
+         })
+    cloths_type = ['shirt', 'trousers']
+    color_type = ['white', 'blue']
+    CODE = ['my' + str(len(MyClothes['CODE']) + 1 + i) for i in range(len(cloths_type))]
+    IDList = ['dummy' + str(i) for i in range(1, 101)]
+    ID = [random.choice(IDList)] * len(cloths_type)
+    BuyDate = [datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') for i in range(len(cloths_type))]
+    myImg = ['img' + str(len(MyClothes['CODE']) + 1) for i in range(len(cloths_type))]
+
+    MyClothes_add = pd.DataFrame(
+        {'CODE': CODE,
+         'ID': ID,
+         'myColor': color_type,
+         'myCategory': cloths_type,
+         'myImg': myImg,
+         'BuyDate': BuyDate
+         })
+    MyClothes = pd.concat([MyClothes, MyClothes_add])
+    return render(request, 'test1.html', context)
--- bigdata/dummydata/dummyMyClothes.py
+++ bigdata/dummydata/dummyMyClothes.py
@@ -1,14 +1,12 @@
 import pandas as pd
 import random
 
-
-MyClothes = pd.read_csv('../dummydata/dummyMyClothes.csv', encoding='Utf-8', index_col=0)
 CODE = ['my'+str(i) for i in range(1,2001)]
 ClothesCODEList = ['CODE'+str(i) for i in range(1,101)]
 IDList = ['dummy'+str(i) for i in range(1,101)]
 ID = [random.choice(IDList) for i in range(1, 2001)]
 BuyDateList = pd.date_range(start='20180101', end='20211231').strftime('%Y-%m-%d %H:%M:%S')
-BuyDate = [random.choice(BuyDateList) for i in range(1,2001)]
+BuyDate = [random.choice(BuyDateList) for i in range(1, 2001)]
 colorList = ['red', 'blue', 'white', 'black', 'blue', 'pattern']
 myColor = [random.choice(colorList) for i in range(1, 2001)]
 categoryList = ['short_sleeve_top', 'long_sleeve_top', 'short_sleeve_outer', 'long_sleeve_outer',
