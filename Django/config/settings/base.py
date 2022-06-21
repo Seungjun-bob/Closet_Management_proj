80cf1935e46f013d611821ed173849feea957d8c
Seungjub-bob <rkdtmdwns0105@gmail.com>
2022-06-21 17:03:28 +0900

    UserData 생성

--- Django/config/settings/base.py
+++ Django/config/settings/base.py
@@ -64,6 +64,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'recommend',
     'rest_framework',
+    'login',
+    'register',
 
     #### Project Apps ####
     'image',
--- Django/register/models.py
+++ Django/register/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class UserData(models.Model):
+    GENDER_CHOICES=(
+        (1,'남성'),
+        (2,'여성'),
+        )
+    email = models.CharField(max_length=40)
+    pw = models.CharField(max_length=40)
+    name = models.CharField(max_length=10)
+    birth = models.DateField()
+    gender = models.IntegerField(choices=GENDER_CHOICES)
