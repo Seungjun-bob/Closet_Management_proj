11704d4afdc42a6a40355df97002acd466e4e8f0
sys8517 <sys8517@naver.com>
2022-06-22 10:20:12 +0900

    http 작업

--- IoTApp/app/src/main/java/com/example/smartcloset/compare/CompareFragment.kt
+++ IoTApp/app/src/main/java/com/example/smartcloset/compare/CompareFragment.kt
@@ -5,11 +5,13 @@ import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.ImageDecoder
 import android.icu.text.SimpleDateFormat
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -22,8 +24,12 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import com.example.smartcloset.MainActivity
 import com.example.smartcloset.R
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.compare.*
 import kotlinx.android.synthetic.main.compare.view.*
+import okhttp3.*
+import org.json.JSONObject
+import kotlin.concurrent.thread
 
 
 class CompareFragment: Fragment() {
@@ -32,6 +38,7 @@ class CompareFragment: Fragment() {
     val PERMISSION_CAMERA = 1001 //맞나?
     val REQUEST_CAMERA = 2 //맞나?
     lateinit var realUri:Uri
+    var img_name = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -116,6 +123,10 @@ class CompareFragment: Fragment() {
     private fun newFileName(): String {
         val sdf = SimpleDateFormat("yyyyMMdd_HHmmss")
         val filename = sdf.format(System.currentTimeMillis())
+        Log.d("img_name", filename)
+        //여기서 이미지 이름을 http로 보내줘야 할 거 같음
+        img_name = filename
+//        sendImgName(img_name)
         return "$filename.jpg"
     }
 
@@ -127,6 +138,7 @@ class CompareFragment: Fragment() {
     }
 
     //Launcher (ActivityResultLuncher로 변경해야함)
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
@@ -136,6 +148,16 @@ class CompareFragment: Fragment() {
                     realUri?.let { uri ->
                         //사진이 찍히면 이미지뷰에 띄워줌, DB로 전송도 해서 값을 받아와 밑의 RecyclerViewㅇ[ 보여줘야함
                         img_compare_preview.setImageURI(uri)
+                        //rest 사용해서 이미지 이름을 보내주고, 스토리지에 이미지를 저장, 이미지는 어떻게 저장?
+//                        img_name = realUri.lastPathSegment.toString() //보내줄 이미지 이름
+
+                        var bitmap = ImageDecoder.decodeBitmap(ImageDecoder.createSource(mainActivity.contentResolver, uri))
+                        Log.d("bitmap", bitmap.toString())
+
+                        Log.d("tt", img_name)
+                        Log.d("api", realUri.toString())
+                        Toast.makeText(context, img_name, Toast.LENGTH_LONG).show()
+
 
                     }
                 }
@@ -169,5 +191,44 @@ class CompareFragment: Fragment() {
 
 
     }
+
+    fun sendImgName(name:String){
+        thread{
+            //이미지 이름을 url 뒤에 붙여 전달해줌
+            var jsonobj = JSONObject()
+            jsonobj.put("ImgName","https://closetimg103341-dev.s3.us-west-2.amazonaws.com/$name" )
+            val url = "http://192.168.200.107:8000/login"  //장고 서버 주소..? 랑 뭘 넣어야하지? view 함수에 들어갈 ~
+
+            //Okhttp3라이브러리의 OkHttpClient객체를 이요해서 작업
+            val client = OkHttpClient()
+
+            //json데이터를 이용해서 request 처리
+            val jsondata = jsonobj.toString()
+            //서버에 요청을 담당하는 객체
+            val builder = Request.Builder()    // request객체를 만들어주는 객체 생성
+            builder.url(url)                   //Builder객체에 request할 주소(네트워크상의 주소)셋팅
+            builder.post(RequestBody.create(MediaType.parse("application/json"),jsondata)) // 요청메시지 만들고 요청메시지의 타입이 json이라고 설정
+            val myrequest: Request = builder.build() //Builder객체를 이용해서 request객체 만들기
+            //생성한 request 객체를 이용해서 웹에 request하기 - request결과로 response 객체가 리턴
+            // ==> Response가 서버에서 돌려준 josn 객체인가??
+            val response: Response = client.newCall(myrequest).execute()
+
+            //response에서 메시지꺼내서 로그 출력하기 -> 결과가 뭘로 오는지, 이미지 이름과 카테고리 분류된 결과가 오면 DB에 저장하는 코드 작성
+            //결과를 받아와서 모델 객체를.. 만들어서? recycler View에 반영해줘야 함
+            val result:String? = response.body()?.string()
+
+            Log.d("http",result!!) //로그 찍어본 후에 파싱해서 옷 객체로 만들고, 리사이클러뷰에 띄우기
+
+            //여기서 데이터 파싱 후 옷 모델 만들어주기? 해야함
+
+            //옷 모델을 만들어서 리사이클러뷰에 넣어줘야함 (= 배열로 만들어서?)
+
+            mainActivity.runOnUiThread {
+                //여기서 리사이클러뷰를 바꿔줘야 하나?
+            }
+
+        }
+    }
+
 
 }
