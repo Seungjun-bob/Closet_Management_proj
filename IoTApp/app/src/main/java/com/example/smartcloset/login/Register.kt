1ae74e5ac68213fd59d5f069ae1bcabf45aa4f5c
Seungjub-bob <rkdtmdwns0105@gmail.com>
2022-06-22 17:05:53 +0900

    register edit

--- IoTApp/app/src/main/java/com/example/smartcloset/login/Register.kt
+++ IoTApp/app/src/main/java/com/example/smartcloset/login/Register.kt
@@ -91,7 +91,7 @@ class Register: AppCompatActivity() {
                               jsonobj.put("gender",1)
                             } else if(gen=="여자") {
                               jsonobj.put("gender",2)
-                            }                      
+                            }
                             val url = "http://172.30.1.47:8000/register/" // 장고 로그인페이지 url - 나중에 수정
 
                             //Okhttp3라이브러리의 OkHttpClient객체를 이요해서 작업
@@ -116,32 +116,6 @@ class Register: AppCompatActivity() {
                     runOnUiThread {
                         Toast.makeText(this, "회원가입 성공", Toast.LENGTH_LONG).show()
                     }
-
-                        // 장고 페이지 url - 나중에 수정
-                        val url = "http://192.168.200.107:8000/register" // 장고 로그인페이지 url - 수정필요
-
-                        //Okhttp3라이브러리의 OkHttpClient객체를 이용해서 작업
-                        val client = OkHttpClient()
-
-                        //json데이터를 이용해서 request 처리
-                        val jsondata = jsonobj.toString()
-                        //서버에 요청을 담당하는 객체
-                        val builder = Request.Builder()    // request객체를 만들어주는 객체 생성
-                        builder.url(url)                   //Builder객체에 request할 주소(네트워크상의 주소)셋팅
-                        builder.post(RequestBody.create(MediaType.parse("application/json"),jsondata)) // 요청메시지 만들고 요청메시지의 타입이 json이라고 설정
-                        val myrequest: Request = builder.build() //Builder객체를 이용해서 request객체 만들기
-                        //생성한 request 객체를 이용해서 웹에 request하기 - request결과로 response 객체가 리턴
-                        val response: Response = client.newCall(myrequest).execute()
-
-                        //response에서 메시지꺼내서 로그 출력하기
-                        val result:String? = response.body()?.string()
-                        Log.d("http",result!!)
-                        //로그인 성공여부가 메시지로 전달되면 그에 따라 다르게 작업할 수 있도록 코드변경하기
-
-                        // 회원가입 성공 토스트 메세지 띄우기
-                        runOnUiThread {
-                            Toast.makeText(this, "회원가입 성공", Toast.LENGTH_LONG).show()
-                        }
 
                         // 로그인 화면으로 이동
                         val intent = Intent(this, FirstLogin::class.java)
