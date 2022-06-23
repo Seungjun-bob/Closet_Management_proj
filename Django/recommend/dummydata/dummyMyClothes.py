9f934d5afcce0a3c93daaecf308482c38ff72cb2
power080900 <power080900@gmail.com>
2022-06-23 11:37:25 +0900

    dummy update

--- Django/recommend/dummydata/dummyMyClothes.py
+++ Django/recommend/dummydata/dummyMyClothes.py
@@ -1,30 +1,27 @@
 import pandas as pd
 import random
 
-
-MyClothes = pd.read_csv('../dummydata/dummyMyClothes.csv', encoding='Utf-8', index_col=0)
-CODE = ['my'+str(i) for i in range(1,2001)]
-ClothesCODEList = ['CODE'+str(i) for i in range(1,101)]
-IDList = ['dummy'+str(i) for i in range(1,101)]
-ID = [random.choice(IDList) for i in range(1,2001)]
-BuyDateList = pd.date_range(start='20180101', end='20211231').strftime('%Y-%m-%d %H:%M:%S')
-BuyDate = [random.choice(BuyDateList) for i in range(1,2001)]
-colorList = ['red', 'blue', 'white', 'black', 'blue', 'pattern']
-myColor = [random.choice(colorList) for i in range(1, 2001)]
-categoryList = ['short_sleeve_top', 'long_sleeve_top', 'short_sleeve_outer', 'long_sleeve_outer',
+code = ['my'+str(i) for i in range(1,2001)]
+IDList = [str(i) for i in range(1,101)]
+id = [random.choice(IDList) for i in range(1, 2001)]
+buydatelist = pd.date_range(start='20180101', end='20211231').strftime('%Y-%m-%d %H:%M:%S')
+buydate = [random.choice(buydatelist) for i in range(1, 2001)]
+colorlist = ['red', 'blue', 'white', 'black', 'blue', 'pattern']
+mycolor = [random.choice(colorlist) for i in range(1, 2001)]
+categorylist = ['short_sleeve_top', 'long_sleeve_top', 'short_sleeve_outer', 'long_sleeve_outer',
                 'vest', 'sling', 'shirts', 'trousers', 'skirt', 'short_sleeve_dress', 'long_sleeve_dress',
                 'vest_dress', 'sling_dress']
-myCategory = [random.choice(categoryList) for i in range(1, 2001)]
-myImg = ['img'+str(i) for i in range(1, 2001)]
+mycategory = [random.choice(categorylist) for i in range(1, 2001)]
+myimg = ['img'+str(i) for i in range(1, 2001)]
 
 
 MyClothes = pd.DataFrame(
-    {'CODE': CODE,
-     'ID': ID,
-     'myColor': myColor,
-     'myCategory': myCategory,
-     'myImg': myImg,
-     'BuyDate': BuyDate
+    {'code': code,
+     'id': id,
+     'mycolor': mycolor,
+     'mycategory': mycategory,
+     'myimg': myimg,
+     'buydate': buydate
      }
 )
 print(MyClothes)
--- bigdata/dummydata/dummyUser.py
+++ bigdata/dummydata/dummyUser.py
@@ -1,24 +1,24 @@
 import pandas as pd
 import random
 
-ID = ['dummy'+str(i) for i in range(1,101)]
-PW = ['dummy'+str(i) for i in range(1,101)]
-PW2 = ['dummy'+str(i) for i in range(1,101)]
-Name = ['dummy'+str(i) for i in range(1,101)]
-dateIndex = pd.date_range(start='19500101', end='20101231')
-print(dateIndex)
-Birth = [random.choice(dateIndex) for i in range(1,101)]
-Email = ['dummy'+str(i)+'@gmail.com' for i in range(1,101)]
+id = [str(i) for i in range(1,101)]
+pw = ['dummy'+str(i) for i in range(1,101)]
+pwcheck = ['dummy'+str(i) for i in range(1,101)]
+name = ['dummy'+str(i) for i in range(1,101)]
+dateindex = pd.date_range(start='19500101', end='20101231')
+print(dateindex)
+birth = [random.choice(dateindex) for i in range(1,101)]
+email = ['dummy'+str(i)+'@gmail.com' for i in range(1,101)]
 GenderList = ['F', 'M']
-Gender = [random.choice(GenderList) for i in range(1,101)]
+gender = [random.choice(GenderList) for i in range(1,101)]
 
 UserData = pd.DataFrame(
-    {'ID': ID,
-     'PW': PW,
-     'PW2': PW2,
-     'Name': Name,
-     'Birth': Birth,
-     'Email': Email,
-     'Gender': Gender})
-print(UserData)
+    {'id': id,
+     'pw': pw,
+     'pwcheck': pwcheck,
+     'name': name,
+     'birth': birth,
+     'email': email,
+     'gender': gender})
+print(UserData.dtypes)
 UserData.to_csv('dummyUser.csv', encoding='Utf-8')
